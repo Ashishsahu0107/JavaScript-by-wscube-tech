4c5ef14279aa2e270aa7986a639d1cdb26ea1e4d
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-04-17 11:54:44 +0530

    Data types, user input, template literals

--- Day1/script.js
+++ Day1/script.js
@@ -1,1 +1,144 @@
-document.write("Ashish")
+// document.write("Ashish")
+// alert("Ashish")
+
+
+// Variables in JavaScript
+
+// A container for storing data
+// Declaring Variables
+// var - old and function-scoped
+// let - block - scoped
+// const - block - scoped, cannot be reassigned
+
+// var name = "Alice"; // function scope
+// let age = 25;
+// const PI = 3.14; // constant
+
+// block scope
+
+// var n = 20;
+// var m = 40;
+
+// console.log(n)
+// console.log(m)
+
+// var n = 70;
+// console.log(n+m) // 110
+
+// var userName = "Ashish sahu"
+// console.log(userName)
+
+
+
+// let
+
+// let a = 80;
+
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+
+// a = 20;
+// console.log(a)
+
+// {
+//     let b = 78;
+
+//     console.log(b)
+// } 
+
+
+// const z = 90;
+// console.log(z)
+
+
+// Question 
+
+// {
+//     const n = 90;
+// }
+
+// var n =90;
+
+// hoisting 
+ 
+
+// Template literals
+
+// Template literals use backticks(') to create strings with embedded
+// variables and support multi - line formatting in JavaScript.
+// let n = 10;
+// console.log(`welcome to ${n} Ashish`)
+
+// console.log(
+//     `
+//         1 name
+//         2 Email
+//         3 Password
+//     `
+// )
+
+// Data Types in Js
+
+// Primitive Data Types:
+// .String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// let n = 90
+
+// console.log(n,typeof(n));
+
+// let m = 90.9
+
+// console.log(m,typeof(m));
+
+
+// Non - Primitive(Reference) Types:
+// .Object, Array, Function
+
+// let l = [10,20,30,40,50]
+// console.log(l, typeof(l));
+
+// let obj = {
+//     name: "pradeep",
+//     age: 56,
+//     name: "Ashish"
+// }
+
+// console.log(obj, typeof(obj));
+
+// function showData() {
+//     let name = "Ashish"
+//     console.log(name);
+    
+// }
+// console.log(showData);
+
+
+// let n = 90;
+// console.log(n * "2"); // number 10 * string 10
+
+// console.log(10 + 20);
+
+// console.log(n + "20") // Append 9020
+
+
+// user input
+// let num1 = prompt("Enter the value: " )
+// console.log(num1);
+
+// string to number conversion
+// let num2 = parseInt(prompt("Enter the value: "))
+// console.log(num2);
+
+// let num3 = Number(prompt("Enter the value: "))
+// console.log(num3);
+
+
+
+
+
+
+
+
