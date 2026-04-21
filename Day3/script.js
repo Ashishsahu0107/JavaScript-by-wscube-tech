86c967e35bcca65b4989ea73a943dafd8c78eb29
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-04-21 16:51:27 +0530

    practice javascript reverse a number

---
+++ Day3/script.js
@@ -0,0 +1,20 @@
+// let num = 12345
+
+// let revNumber = 0
+
+// while (num > 0) {
+//     let lastDigit = num % 10
+//     revNumber = revNumber * 10 + lastDigit
+//     num = parseInt(num / 10)
+// }
+
+// console.log(revNumber);
+
+
+
+for(let i=1; i<=10; i++){
+    for(let j=0; j<=j/2; j++){
+        document.write(i);
+    }
+    document.write("<br>")
+}
